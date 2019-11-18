678810786a42f2afad52daafba306177d69ea691
nosenko.a <nosenko.a@dbbest.com>
2019-11-18 11:00:19 +0200

    [*] Task 5294(completed) Student Project. Add Reselect Selectors. Optimize nodes fetching from store using Reselect library.

--- anatolii-nosenko-student-project/frontend/src/selectors/selectors.js
+++ anatolii-nosenko-student-project/frontend/src/selectors/selectors.js
@@ -1,3 +1,5 @@
+import {createSelector} from "reselect";
+
 export function getRootNodeIdSelector(state) {
     return {
         id: state.tree.rootId,
@@ -20,11 +22,16 @@ export function getConnectionPropertiesSelector(state) {
     }
 }
 
-export function findTheNodeSelector(state, id) {
-    return {
-        node: state.tree.nodes.find(node => node.item.uniqueId === id)
-    }
-}
+const selectNodes = state => state.tree.nodes;
+const selectId = (state, nodeId) => nodeId;
+
+export const findTheNodeSelector = createSelector(
+        [selectNodes, selectId],
+        (nodes, id) => {
+            return {
+                node: nodes.find(node => node.item.uniqueId === id)
+            }
+        });
 
 export function getItemToShow(state) {
     return {
