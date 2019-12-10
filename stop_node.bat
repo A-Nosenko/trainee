4fb3b865003be6b58734b27ec22c132c50fc4d98
nosenko.a <nosenko.a@dbbest.com>
2019-12-10 12:01:37 +0200

    [*/-] Task 5307(in process) Student Project. Learning Jenkins use cases. Fix bat files.

--- stop_node.bat
+++ stop_node.bat
@@ -1,4 +1,6 @@
-IF EXIST node.exe (taskkill /F /IM node.exe) ELSE (echo '== process already stopped ==')
+tasklist /FI "IMAGENAME eq node.exe" 2>NUL | find /I /N "node.exe">NUL
+if "%ERRORLEVEL%"=="0" (taskkill /F /IM node.exe)
+
 
 
 
--- stop_tomcat.bat
+++
@@ -1,7 +0,0 @@
-%@Try%
-for /f "tokens=5" %a in ('netstat -aon ^| find ":8080" ^| find "LISTENING"') do taskkill /f /pid %a
-%@EndTry%
-:@Catch
-exit 0
-:@EndCatch
-
