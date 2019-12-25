f335f28fd34d9f75379ed471a02f66a970583fd7
nosenko.a <nosenko.a@dbbest.com>
2019-12-25 20:38:33 +0200

    breadcrumb with download button

--- anatolii-nosenko-student-project/frontend/src/components/components.js
+++ anatolii-nosenko-student-project/frontend/src/components/components.js
@@ -4,18 +4,22 @@ import ConnectionForm from './ConnectionForm';
 import TreeView from './TreeView';
 import StatusBar from './StatusBar';
 import Breadcrumb from "./breadcrumb/Breadcrumb";
+import './components.css'
 
 const Header = () =>
-    <header>
-        <StatusBar/>
-        <nav className='App-nav'>
-            <Link to="connection">Connection</Link>
-            <Link to="tree">Database Tree</Link>
-            <Link to="about">About Program</Link>
-        </nav>
-
-        <Breadcrumb/>
-    </header>;
+    <div>
+        <header>
+            <StatusBar/>
+            <nav className='App-nav'>
+                <Link to="connection">Connection</Link>
+                <Link to="tree">Database Tree</Link>
+                <Link to="about">About Program</Link>
+            </nav>
+        </header>
+        <div className='breadcrumb_container'>
+            <Breadcrumb/>
+        </div>
+    </div>;
 
 export const Home = () =>
     <div>
@@ -44,10 +48,15 @@ export const Tree = () =>
 export const About = () =>
     <div>
         <Header/>
-        <div className='Center-container'>
-            <h2>Database Viewer 2019. Supports working with MySQL database.</h2>
-            <h2>Version 0.0.8</h2>
-		<h1>I am happy!!!</h1>
+        <div>
+            <div className='button_container'>
+                <button className='btn btn-primary'><i className="fas fa-arrow-down"/> Download</button>
+            </div>
+            <div className='Center-container'>
+                <h2>Database Viewer 2019. Supports working with MySQL database.</h2>
+                <h2>Version 0.0.8</h2>
+                <h1>I am happy!!!</h1>
+            </div>
         </div>
     </div>;
 
